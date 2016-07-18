3abbd5b2d04938c366a292a725511c4bdcaaaee7
nov <nov@matake.jp>
2016-07-18 17:44:36 +0900

    register static clients and debugger

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@ Scope.create [
   {name: 'address'},
   {name: 'phone'  }
 ]
+
+Client.create [{
+  identifier: '8d6c384015481a2e1e6151bcd283a64d',
+  secret: 'ad1d3c5c4948fb0b06952c5075e53ed46ba86d93c1d501633cc4fd24a99c229b',
+  name: 'RP (response_type=code)',
+  redirect_uri: 'http://sec-rp-code.dev/callback'
+}, {
+  identifier: 'ec427a44c1076c0653a48f6162b044d6',
+  secret: '932692979933e82156199ea051564e1914809af3255f35b82cae0cf016d6c69f',
+  name: 'RP (response_type=token)',
+  redirect_uri: 'http://sec-rp-token.dev/callback'
+}, {
+  identifier: 'c1e8e740a49e327231da8e844fb161bf',
+  secret: 'c10382b806f513c49059aca5e2696a621a9dbeef5555fa628c5d1619aa442a5d',
+  name: 'RP (response_type=id_token)',
+  redirect_uri: 'http://sec-rp-id-token.dev/callback'
+}]
